d7f6954d6853b8760b8df3727ae2693edeaacb7a
darxtorm <joel.j.devlin@gmail.com>
2015-12-02 12:39:14 +1100

    initial updates

--- removecompletedtorrents.sh
+++ removecompletedtorrents.sh
@@ -1,17 +1,35 @@
 #!/bin/sh
-TORRENTLIST=`transmission-remote --auth=user:password --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=' ' --fields=1`
+## Made by JD from these two scripts, good for outputting results to a file as a scheduled task
+## lacks the --auth=user:password as i don't use auth on my server
+## http://goedonthouden.com/2013/04/21/transmission-daemon-auto-remove-torrents/
+## https://gist.github.com/bulljit/791609
+TORRENTLIST=`transmission-remote --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=' ' --fields=1`
+echo "Getting torrent list from transmission"
 
 for TORRENTID in $TORRENTLIST
 do
-echo "* * * * * Operations on torrent ID $TORRENTID starting. * * * * *"
+## found these too noisy for output
+#echo "* * * * * Operations on torrent ID $TORRENTID starting. * * * * *"
 
-DL_COMPLETED=`transmission-remote --auth=user:password --torrent $TORRENTID --info | grep "Percent Done: 100%"`
-if [ "$DL_COMPLETED" != "" ]; then
-echo "Torrent #$TORRENTID is completed."
-echo "Removing torrent from list."
-transmission-remote --auth=user:password --torrent $TORRENTID --remove
+NAME_IRREGARDLESS=`transmission-remote --torrent $TORRENTID --info | grep "Name:"`
+DL_IRREGARDLESS=`transmission-remote --torrent $TORRENTID --info | grep "Percent Done:"`
+DL_COMPLETED=`transmission-remote --torrent $TORRENTID --info | grep "Percent Done: 100%"`
+STATE_IRREGARDLESS=`transmission-remote --torrent $TORRENTID --info | grep "State:"`
+## commented out and replaced below as i only want finished torrents, you may edit accordingly
+#STATE_STOPPED=`transmission-remote --torrent $TORRENTID --info | grep "State: Stopped\|Finished\|Idle"`
+STATE_STOPPED=`transmission-remote --torrent $TORRENTID --info | grep "State: Finished"`
+echo "#$TORRENTID $NAME_IRREGARDLESS"
+
+if [ "$DL_COMPLETED" != "" ] && [ "$STATE_STOPPED" != "" ]; then
+echo "Torrent #$TORRENTID is $DL_COMPLETED and $STATE_STOPPED."
+echo "Removing torrent #$TORRENTID from list."
+#transmission-remote --torrent $TORRENTID --remove
+echo "JOKES NOT REALLY, UNCOMMENT LINE ABOVE TO GIVE ME TEETH"
+
 else
-echo "Torrent #$TORRENTID is not completed. Ignoring."
+echo "Torrent #$TORRENTID is not completed. $DL_IRREGARDLESS $STATE_IRREGARDLESS"
 fi
-echo "* * * * * Operations on torrent ID $TORRENTID completed. * * * * *"
+
+## found these too noisy for output
+#echo "* * * * * Operations on torrent ID $TORRENTID completed. * * * * *"
 done
