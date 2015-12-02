776f757d92b1561bf00903ed62a15d7c0b03d600
darxtorm <joel.j.devlin@gmail.com>
2015-12-02 15:13:50 +1100

    cleanup

--- removecompletedtorrents.sh
+++ removecompletedtorrents.sh
@@ -3,30 +3,25 @@
 ## lacks the --auth=user:password as i don't use auth on my server
 ## http://goedonthouden.com/2013/04/21/transmission-daemon-auto-remove-torrents/
 ## https://gist.github.com/bulljit/791609
-TORRENTLIST=`transmission-remote --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=' ' --fields=1`
 echo "Getting torrent list from transmission"
-
+TORRENTLIST=`transmission-remote --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=' ' --fields=1`
 for TORRENTID in $TORRENTLIST
 do
-
-# grab a bunch of info about the torrent
-NAME_IRREGARDLESS=`transmission-remote --torrent $TORRENTID --info | grep "Name:"`
-DL_IRREGARDLESS=`transmission-remote --torrent $TORRENTID --info | grep "Percent Done:"`
-DL_COMPLETED=`transmission-remote --torrent $TORRENTID --info | grep "Percent Done: 100%"`
-STATE_IRREGARDLESS=`transmission-remote --torrent $TORRENTID --info | grep "State:"`
-## commented out and replaced below as i only want finished torrents, you may edit accordingly
-#STATE_STOPPED=`transmission-remote --torrent $TORRENTID --info | grep "State: Stopped\|Finished\|Idle"`
-STATE_STOPPED=`transmission-remote --torrent $TORRENTID --info | grep "State: Finished"`
-echo "#$TORRENTID $NAME_IRREGARDLESS"
-
-# check if the torrent is complete and is in finished state and remove
-if [ "$DL_COMPLETED" != "" ] && [ "$STATE_STOPPED" != "" ]; then
-echo "Torrent #$TORRENTID is $DL_COMPLETED and $STATE_STOPPED."
-echo "Removing torrent #$TORRENTID from list."
-transmission-remote --torrent $TORRENTID --remove
-
-else
-echo "Torrent #$TORRENTID is not completed. $DL_IRREGARDLESS $STATE_IRREGARDLESS"
-fi
-
+  # grab a bunch of info about the torrent
+  NAME_IRREGARDLESS=`transmission-remote --torrent $TORRENTID --info | grep "Name:"`
+  DL_IRREGARDLESS=`transmission-remote --torrent $TORRENTID --info | grep "Percent Done:"`
+  DL_COMPLETED=`transmission-remote --torrent $TORRENTID --info | grep "Percent Done: 100%"`
+  STATE_IRREGARDLESS=`transmission-remote --torrent $TORRENTID --info | grep "State:"`
+  STATE_STOPPED=`transmission-remote --torrent $TORRENTID --info | grep "State: Finished"`
+  ## commented out and replaced below as i only want finished torrents, you may edit accordingly
+  #STATE_STOPPED=`transmission-remote --torrent $TORRENTID --info | grep "State: Stopped\|Finished\|Idle"`
+  echo "#$TORRENTID $NAME_IRREGARDLESS"
+  # check if the torrent is complete and is in finished state and remove
+  if [ "$DL_COMPLETED" != "" ] && [ "$STATE_STOPPED" != "" ]; then
+    echo "Torrent #$TORRENTID is $DL_COMPLETED and $STATE_STOPPED."
+    echo "Removing torrent #$TORRENTID from list."
+    transmission-remote --torrent $TORRENTID --remove
+  else
+    echo "Torrent #$TORRENTID is not completed. $DL_IRREGARDLESS $STATE_IRREGARDLESS"
+  fi
 done
